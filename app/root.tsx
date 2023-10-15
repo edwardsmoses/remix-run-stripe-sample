aa2c624a1a3a9a8e2782b07cc11d5646fcc2db25
Edwards Moses <edwardsmoses3@gmail.com>
2023-10-15 08:56:04 +0100

    feat: install remix dev tools

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
